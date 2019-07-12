4e25bd2bc3294f7d4842087c838555f91c7e7a57
mrandrsliamrussell <mrandmrsliamrussell@gmail.com>
2019-07-12 08:58:23 +0100

    k
    k

---
+++ lab06/SQLQuery6.sql
@@ -0,0 +1,13 @@
+﻿/*select p.SupplierID, companyname, AVG(unitsonorder) as averageorder
+from products p
+inner join Suppliers s on p.SupplierID = s.SupplierID
+group by p.SupplierID, CompanyName having AVG(unitsonorder) > 0
+order by AVG(unitsonorder) desc*/
+
+/*select * from products  inner join Suppliers on Products.SupplierID = Suppliers.SupplierID order by ProductID
+
+select * from products  right join Suppliers on Products.SupplierID = Suppliers.SupplierID order by ProductID*/
+
+select * from products 
+inner join Suppliers on Products.SupplierID = suppliers.SupplierID 
+inner join Categories on Products.CategoryID = Categories.CategoryID
